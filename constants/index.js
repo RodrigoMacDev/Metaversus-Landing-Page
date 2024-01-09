22cd4f341a20f41ca667c743c5f995d1b268fc8d
Rodrigo Macedo <rodmacdev@gmail.com>
2024-01-08 21:32:29 -0300

    first commit

---
+++ constants/index.js
@@ -0,0 +1,87 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'Mundo Invertido',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Kadirojo Permai',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Ilha do Paraíso',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Laboratório Hawkins',
+  },
+];
+
+export const startingFeatures = [
+  'Ache um mundo que tem o que deseja e entre',
+  'Entre no novo mundo de forma segura',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'Novo mundo',
+    subtitle:
+        'Nós temos novas atualizações com novos mundos para você',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'Mais realista',
+    subtitle:
+        'Na última atualização, seus olhos ficam mais estreitos, fazendo o mundo mais realista do que nunca.',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'O lançamento do Metaverso e um mundo de viagens interplanetárias',
+    subtitle:
+        'Explore a galáxia e descubra lugares jamais vistos antes.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 dicas para facilmente dominar qualquer planeta do Metaverso',
+    subtitle:
+        'Planetas paradisíacos com recursos exóticos e vida alienígena',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Com uma plataforma você pode explorar o mundo inteiro virtualmente',
+    subtitle:
+        'Com uma nave escolhida por você para se aventurar em tudo o que o Metaverso pode lhe oferecer',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
